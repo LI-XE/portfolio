87fccaec4d1f95a0b5e9620581397f7dbce1f0d7
LI_XE <hyangeun312@gmail.com>
2021-10-08 01:12:32 -0500

    navbar completed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from './components/Navbar';
+
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
+      <Navbar />  
   );
 }
 
